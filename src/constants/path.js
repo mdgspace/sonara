657449cb1218942ee0270f4c22be3b0f2ef8ce59
Budhayan Chakraborty <budhayanc2005@gmail.com>
2025-10-03 22:25:38 +0530

    fix(image): Display wave image correctly

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,7 +1,11 @@
+import sine from '../assets/sine.png';
+import square from '../assets/square.png';
+import triangle from '../assets/triangle.png';
+import sawtooth from '../assets/sawtooth.png';
 
 export const waveimages = {
-    sine: '../assets/sine.png',
-    square: '../assets/square.png',
-    triangle: '../assets/triangle.png',
-    sawtooth: '../assets/sawtooth.png'
+    sine,
+    square,
+    triangle,
+    sawtooth
 };
