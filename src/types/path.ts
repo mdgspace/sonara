075b0b238680444fa3f61898d8e3ab0bfc39a754
Anurag-Bansode <anuragnbansode@gmail.com>
2025-10-14 19:45:46 +0530

    refactor(types): organize and expand synthesizer type definitions
    Why:
    Improves the organization of files and is easier to debug
    
    What:
    - The main `src/types/index.ts` acts as main source
    - Created individual type definition files for each synthesizer module (`envelope.ts`, `filter.ts`, `lfo.ts`, `oscillator.ts`, `effects.ts`, `sequencer.ts`).
    - Expanded the core `SynthState` interface to include modules mentioned in readme .
    - Added new interfaces for `Oscillator` and `SequencerStep`.
    - Converted the `path.js` constants file to `path.ts` to ensure type-safe handling of asset paths.
    
     Changes to be committed:
    	new file:   src/types/effects.ts
    	new file:   src/types/envelope.ts
    	new file:   src/types/eq.ts
    	new file:   src/types/filter.ts
    	new file:   src/types/index.ts
    	new file:   src/types/lfo.ts
    	new file:   src/types/oscillator.ts
    	new file:   src/types/path.ts
    	new file:   src/types/sequencer.ts
    	new file:   src/types/waveform.ts

---
+++ src/types/path.ts
@@ -0,0 +1,12 @@
+import sine from '../assets/sine.png';
+import square from '../assets/square.png';
+import triangle from '../assets/triangle.png';
+import sawtooth from '../assets/sawtooth.png';
+import type { WaveformType } from '@/types';
+
+export const waveimages: Record<WaveformType, string> = {
+  sine,
+  square,
+  triangle,
+  sawtooth,
+};
